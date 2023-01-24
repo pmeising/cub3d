a814ca760d9c1788c159a696ba9ce7188a0dd49c
pmeising <pmeising@student.42wolfsburg.de>
2023-01-24 10:14:20 +0100

    Fixed the sideways movement

--- src/ft_move.c
+++ src/ft_move.c
@@ -6,7 +6,7 @@
 /*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 17:21:53 by pmeising          #+#    #+#             */
-/*   Updated: 2023/01/13 19:47:38 by pmeising         ###   ########.fr       */
+/*   Updated: 2023/01/24 10:12:15 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,26 @@ void	ft_movement_s(t_prgrm *vars)
 		(int)vars->playa[0]] == '0' || vars->map[(int)((vars->playa[1] + \
 		(-vars->dir[1] * SPEED)))][(int)vars->playa[0]] == 'O')
 			vars->playa[1] = vars->playa[1] + (-vars->dir[1] * SPEED);
+}
+
+void	ft_movement_d(t_prgrm *vars)
+{
+	if (vars->map[(int)vars->playa[1]][(int)(vars->playa[0] + (vars->camera_vector[0] \
+		* (double)SPEED))] == '0')
+		vars->playa[0] = vars->playa[0] + (vars->camera_vector[0] * (double)SPEED);
+	if (vars->map[(int)((vars->playa[1] + (vars->camera_vector[1] * (double)SPEED)))][\
+		(int)vars->playa[0]] == '0')
+			vars->playa[1] = vars->playa[1] + (vars->camera_vector[1] * SPEED);
+}
+
+void	ft_movement_a(t_prgrm *vars)
+{
+	if (vars->map[(int)vars->playa[1]][(int)(vars->playa[0] + (-vars->camera_vector[0] \
+		* (double)SPEED))] == '0')
+		vars->playa[0] = vars->playa[0] + (-vars->camera_vector[0] * (double)SPEED);
+	if (vars->map[(int)((vars->playa[1] + (-vars->camera_vector[1] * (double)SPEED)))][\
+		(int)vars->playa[0]] == '0')
+			vars->playa[1] = vars->playa[1] + (-vars->camera_vector[1] * (double)SPEED);
 }
 
 /*
@@ -89,9 +109,11 @@ int	ft_movement(int keycode, t_prgrm *vars)
 	else if (keycode == 115)
 		ft_movement_s(vars);
 	else if (keycode == 97)
-		ft_rotate_fov(vars, 1);
+		ft_movement_a(vars);
+		// ft_rotate_fov(vars, 1);
 	else if (keycode == 100)
-		ft_rotate_fov(vars, -1);
+		ft_movement_d(vars);
+		// ft_rotate_fov(vars, -1);
 	else if (keycode == 32)
 		ft_open_door(vars);
 	ft_raycast(vars);
